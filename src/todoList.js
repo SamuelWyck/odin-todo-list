ef32c2fc0a4ac00649038dd5e257f8776b8e9226
samuel <samawyck@gmail.com>
2025-02-20 13:42:04 -0600

    Create todoList IIFE

---
+++ src/todoList.js
@@ -0,0 +1,66 @@
+import createProject from "./project.js";
+
+
+const todoList = (function() {
+    const defaultProjectId = 0;
+    let projectList = [];
+    projectList.push(createProject("General Tasks", setId=defaultProjectId));
+
+
+    let addProject = function(title) {
+        const project = createProject(title);
+        projectList.push(project);
+    };
+
+    let removeProject = function(id) {
+        if (id === defaultProjectId) {
+            return;
+        }
+        let newProjectList = []
+
+        for (let project of projectList) {
+            if (project.id !== id) {
+                newProjectList.push(project);
+            }
+        }
+
+        projectList = newProjectList;
+    };
+
+    let editProjectTitle = function(id, title) {
+        if (id === defaultProjectId) {
+            return false;
+        }
+        for (let project of projectList) {
+            if (project.id === id) {
+                project.title = title;
+                return true;
+            }
+        }
+        return false;
+    };
+
+    let getProject = function(id) {
+        for (let project of projectList) {
+            if (project.id === id) {
+                return project;
+            }
+        }
+        return null;
+    };
+
+    let getProjectList = function() {
+        return projectList;
+    };
+
+    return {
+        "addProject": addProject,
+        "removeProject": removeProject,
+        "editProjectTitle": editProjectTitle,
+        "getProject": getProject,
+        "getProjectList": getProjectList,
+    };
+})();
+
+
+export default todoList;
