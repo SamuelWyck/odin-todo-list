2a5b940ac75bef81a9f317c5789ebba129cc41d6
samuel <samawyck@gmail.com>
2025-02-21 09:58:10 -0600

    Add motheds to add and remove tasks and refactor key forming

--- src/localStorage.js
+++ src/localStorage.js
@@ -3,18 +3,38 @@
 const storageManager = (function() {
 
     const projectKeyPrefix = "p-";
+    const taskKeyPrefix = "t-";
 
     
+    let getProjectKey = function(projectId) {
+        return projectKeyPrefix + String(projectId);
+    };
+
+    let getTaskKey = function(taskId, projectId) {
+        return taskKeyPrefix + String(taskId) + String(projectId);
+    };
+
     let addProject = function(project) {
-        const id = project.id;
         const JSONData = JSON.stringify(project);
-        const key = projectKeyPrefix + String(id);
+        const key = getProjectKey(project.id);
 
         localStorage.setItem(key, JSONData);
     };
 
     let removeProject = function(projectId) {
-        const key = projectKeyPrefix + String(projectId);
+        const key = getProjectKey(projectId);
+        localStorage.removeItem(key);
+    };
+
+    let addTask = function(projectId, task) {
+        const JSONData = JSON.stringify(task);
+        const key = getTaskKey(task.id, projectId);
+
+        localStorage.setItem(key, JSONData);
+    };
+
+    let removeTask = function(taskId, projectId) {
+        const key = getTaskKey(taskId, projectId);
         localStorage.removeItem(key);
     };
 
