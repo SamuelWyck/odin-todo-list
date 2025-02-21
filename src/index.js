9297ef32fa7a53cca85877534aede2814154033a
samuel <samawyck@gmail.com>
2025-02-21 12:38:07 -0600

    Add test code

---
+++ src/index.js
@@ -0,0 +1,32 @@
+// import storageManager from "./localStorage.js";
+// import createProject from "./project.js";
+// import Task from "./task.js";
+
+
+// const project = createProject("test");
+
+// const task = project.addTask("task", "we we", 2025, 2, 23, 0);
+
+// storageManager.addProject(project)
+
+// console.log(project.taskList)
+
+// const projects = storageManager.getProjects();
+
+// console.log(projects)
+
+// const date = projects[0].taskList[0].dueDate
+
+// console.log(date)
+
+// const newTask = new Task("test1", "we", 0, 0, 0, 0)
+// newTask.dueDate = new Date(date)
+// console.log(newTask.dueDate)
+// console.log(newTask.getDueDate())
+
+
+
+
+
+// localStorage.clear()
+
