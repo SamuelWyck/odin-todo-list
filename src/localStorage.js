bb75698934e57880cc1cd15d686faf477a0a4ae2
samuel <samawyck@gmail.com>
2025-02-21 11:08:14 -0600

    Temporarily comment out task methods and hack out getProjects method

--- src/localStorage.js
+++ src/localStorage.js
@@ -2,17 +2,18 @@
 
 const storageManager = (function() {
 
-    const projectKeyPrefix = "p-";
-    const taskKeyPrefix = "t-";
+    const projectKeyPrefix = "p";
+    // const taskKeyPrefix = "t";
+    const delimiter = "-";
 
     
     let getProjectKey = function(projectId) {
-        return projectKeyPrefix + String(projectId);
+        return projectKeyPrefix + delimiter + String(projectId);
     };
 
-    let getTaskKey = function(taskId, projectId) {
-        return taskKeyPrefix + String(taskId) + String(projectId);
-    };
+    // let getTaskKey = function(taskId, projectId) {
+    //     return taskKeyPrefix + delimiter + String(taskId) + delimiter + String(projectId);
+    // };
 
     let addProject = function(project) {
         const JSONData = JSON.stringify(project);
@@ -26,17 +27,41 @@ const storageManager = (function() {
         localStorage.removeItem(key);
     };
 
-    let addTask = function(projectId, task) {
-        const JSONData = JSON.stringify(task);
-        const key = getTaskKey(task.id, projectId);
+    // let addTask = function(projectId, task) {
+    //     const JSONData = JSON.stringify(task);
+    //     const key = getTaskKey(task.id, projectId);
 
-        localStorage.setItem(key, JSONData);
+    //     localStorage.setItem(key, JSONData);
+    // };
+
+    // let removeTask = function(taskId, projectId) {
+    //     const key = getTaskKey(taskId, projectId);
+    //     localStorage.removeItem(key);
+    // };
+
+    let getProjects = function() {
+        const projectList = [];
+
+        for (let i = 0; i < localStorage.length; i += 1) {
+            const key = localStorage.key(i);
+            const [prefix, id] = key.split(delimiter);
+            if (prefix !== projectKeyPrefix) {
+                continue;
+            }
+            const JSONData = localStorage.getItem(key);
+            const project = JSON.parse(JSONData);
+            projectList.push(project);
+        }
+
+        return projectList;
     };
 
-    let removeTask = function(taskId, projectId) {
-        const key = getTaskKey(taskId, projectId);
-        localStorage.removeItem(key);
+    return {
+        "addProject": addProject,
+        "removeProject": removeProject,
+        "getProjects": getProjects,
     };
-
-
 })();
+
+
+export default storageManager;
