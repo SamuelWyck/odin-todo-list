6a90e369cd0b271abe2f84df4731a7531b7ef915
samuel <samawyck@gmail.com>
2025-02-24 15:39:09 -0600

    Add ability to pass a task object to form creation to use as input values

--- src/taskForm.js
+++ src/taskForm.js
@@ -27,56 +27,76 @@ const createTaskForm = function() {
         return label;
     };
 
-    let createInput = function(name, id, type) {
+    let createInput = function(value, name, id, type, date=false) {
         const input = document.createElement("input");
         input.name = name;
         input.id = id;
         input.type = type;
+        if (!date) {
+            input.value = value;
+        } else {
+            input.valueAsDate = value;
+        }
         return input;
     };
 
-    let createTitleAndDateSection = function() {
+    let createTitleAndDateSection = function(task) {
         const section = document.createElement("div");
         section.classList.add("title-date-div");
 
+        let titleValue = "";
+        let dateValue = "";
+        if (task !== null) {
+            titleValue = task.title;
+            dateValue = task.dueDate
+        }
+
         const titleDiv = document.createElement("div");
         titleDiv.appendChild(createLabel("Title", "title"));
-        titleDiv.appendChild(createInput("title", "title", "text"));
+        titleDiv.appendChild(createInput(titleValue, "title", "title", "text"));
         section.appendChild(titleDiv);
 
         const dateDiv = document.createElement("div");
         dateDiv.appendChild(createLabel("Date", "date"));
-        dateDiv.appendChild(createInput("date", "date", "date"));
+        dateDiv.appendChild(createInput(dateValue, "date", "date", "date", true));
         section.appendChild(dateDiv);
 
         return section;
     };
 
-    let createOption = function(text, value) {
+    let createOption = function(text, value, priority) {
         const option = document.createElement("option");
         option.value = value;
         option.textContent = text;
+        if (value === priority) {
+            option.selected = true;
+        }
         return option;
     };
 
-    let createSelect = function(name, id) {
+    let createSelect = function(task, name, id) {
         const select = document.createElement("select");
         select.id = id;
         select.name = name;
 
-        select.appendChild(createOption("None", 0));
-        select.appendChild(createOption("Low", 1));
-        select.appendChild(createOption("Medium", 2));
-        select.appendChild(createOption("High", 3));
+        let priority = 0;
+        if (task !== null) {
+            priority = task.priority;
+        }
+
+        select.appendChild(createOption("None", 0, priority));
+        select.appendChild(createOption("Low", 1, priority));
+        select.appendChild(createOption("Medium", 2, priority));
+        select.appendChild(createOption("High", 3, priority));
 
         return select;
     };
 
-    let createPrioritySection = function() {
+    let createPrioritySection = function(task) {
         const section = document.createElement("div");
 
         section.appendChild(createLabel("Priority", "priority"));
-        section.appendChild(createSelect("priority", "priority"));
+        section.appendChild(createSelect(task, "priority", "priority"));
 
         return section;
     };
@@ -95,23 +115,24 @@ const createTaskForm = function() {
         return section;
     };
 
-    let createFormElement = function(edit) {
+    let createFormElement = function(edit, task) {
         const form = document.createElement("form");
         form.action = "#";
 
-        form.appendChild(createTitleAndDateSection());
-        form.appendChild(createPrioritySection());
+        form.appendChild(createTitleAndDateSection(task));
+        form.appendChild(createPrioritySection(task));
+        //textarea
         form.appendChild(createBtnSection(edit));
     };
     
-    let createTaskFormPopup = function(edit=false) {
+    let createTaskFormPopup = function(edit=false, task=null) {
         const body = document.querySelector("body");
         
         const popup = document.createElement("div");
         popup.classList.add("popup");
 
         popup.appendChild(createExitBtnSection());
-        popup.appendChild(createFormElement(edit));
+        popup.appendChild(createFormElement(edit, task));
 
         body.appendChild(popup);
     };
